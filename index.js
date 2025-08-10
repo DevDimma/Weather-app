fe093f082a1274ba62797f511a34de37ed57f3ad
Dev Dimma <elizabethndubuisi8@gmail.com>
2025-08-10 22:25:58 +0100

    changes made

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let city = searchInputElement.value;
+  let apiKey = "a6469t9f026f33001d9f3f695ocb3042";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function formatDate(date) {
